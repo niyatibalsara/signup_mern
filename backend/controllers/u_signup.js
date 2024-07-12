e2c569584293e61dba1e69509983ecbd98d28f79
niyatibalsara <129919078+niyatibalsara@users.noreply.github.com>
2024-07-12 21:37:48 +0530

    first commit

---
+++ backend/controllers/u_signup.js
@@ -0,0 +1,17 @@
+const userModel = require("../models/u_signup");
+
+const sign_up = async (req, res) => {
+  try {
+    const newUser = new userModel({
+      name: req.body.name,
+      password: req.body.password,
+    });
+
+    const result = await newUser.save();
+    res.json(result);
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+module.exports = sign_up;
